c22cbc55101dd89ed6fc8eeef252aba593072e9e
pc-escritorio <marlonaponte001@gmail.com>
2024-12-15 18:48:57 -0500

    act

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import react from "react";
+
 const Header = () =>{
     return (
         <header className="flex flex-col justify-center items-center">
-            <img src="../public/logo.png" alt="logo commitV" className="w-72"/>
+            <img src="/logo.png" alt="logo commitV" className="w-72"/>
             <h1 className="text-4xl  text-center text-sky-300 mt-10 font-bold">MONITOREA TU ULTIMO COMMIT O EL DE TUS COLABORADORES EN GitHub</h1>
         </header>
     );
